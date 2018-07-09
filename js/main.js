07797f40bb0ba93b1d7c26fd197a34f593c00f18
Haoming Wu <felixyonex@126.com>
2018-07-09 21:45:57 +1000

    Content changed. Optimized layout on mobile phone.

---
+++ js/main.js
@@ -0,0 +1,22 @@
+/* 3thPage box height
+The frame is a flex box
+set their height the same as the that of the largest one */
+
+/* (function() {
+    if (document.documentElement.clientWidth > 480) {
+      var frames = $(".frame");
+      var frameHeighArr = [];
+  
+      for (var i = 0, len = frames.length; i < len; i++) {
+        frameHeighArr.push(frames[i].scrollHeight);
+      }
+  
+      var maxHeight = Math.max(...frameHeighArr);
+      for (var i = 0, len = frames.length; i < len; i++) {
+        frames[i].style.height = maxHeight + "px";
+      }
+    } else {
+      return;
+    }
+  })(); */
+  
