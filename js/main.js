5b4c4418612902633972cd33d5e1b69bf0c0f68a
Haoming Wu <felixyonex@126.com>
2018-07-11 20:04:42 +1000

    add js animation to 4th page on mobile device

--- js/main.js
+++ js/main.js
@@ -1,22 +1,12 @@
-/* 3thPage box height
-The frame is a flex box
-set their height the same as the that of the largest one */
+//set animation for arrows in fourth Page when displayed on mobile device
 
-/* (function() {
-    if (document.documentElement.clientWidth > 480) {
-      var frames = $(".frame");
-      var frameHeighArr = [];
-  
-      for (var i = 0, len = frames.length; i < len; i++) {
-        frameHeighArr.push(frames[i].scrollHeight);
-      }
-  
-      var maxHeight = Math.max(...frameHeighArr);
-      for (var i = 0, len = frames.length; i < len; i++) {
-        frames[i].style.height = maxHeight + "px";
-      }
-    } else {
-      return;
-    }
-  })(); */
-  
+  $(".arrowDown").click(function() {
+    $(".firstP").animate({ marginTop: "100%", opacity: 0 }, 500);
+    $(".secondP").slideToggle(500);
+  });
+  $(".arrowUp").click(function() {
+    $(".secondP").slideToggle(500);
+    $(".firstP").animate({ marginTop: "0%", opacity: 1 }, 500);
+  });
+
+
