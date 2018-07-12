86dc38386f6be4fe4a79cf95f4e6fce975508166
Haoming Wu <felixyonex@126.com>
2018-07-12 23:57:06 +1000

    improved avatar display on mobile. Shorten the animation duration on 4th page

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 /* set animation for arrows in fourth Page when displayed on mobile device
  */
   $(".arrowDown").click(function() {
-    $(".firstP").animate({ marginTop: "100%", opacity: 0 }, 500);
-    $(".secondP").slideToggle(500);
+    $(".firstP").animate({ marginTop: "100%", opacity: 0 }, 300);
+    $(".secondP").slideToggle(300);
   });
   $(".arrowUp").click(function() {
-    $(".secondP").slideToggle(500);
-    $(".firstP").animate({ marginTop: "0%", opacity: 1 }, 500);
+    $(".secondP").slideToggle(300);
+    $(".firstP").animate({ marginTop: "0%", opacity: 1 }, 300);
   });
 
 
