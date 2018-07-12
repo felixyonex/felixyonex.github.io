e8b7b5d2458f528d3101c236960a1f5377d376ff
Haoming Wu <felixyonex@126.com>
2018-07-12 19:42:22 +1000

    first page avatar loading optimised. Changed 3rd page HTML nesting construction. Add small changes to the links in 3rd page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-//set animation for arrows in fourth Page when displayed on mobile device
-
+/* set animation for arrows in fourth Page when displayed on mobile device
+ */
   $(".arrowDown").click(function() {
     $(".firstP").animate({ marginTop: "100%", opacity: 0 }, 500);
     $(".secondP").slideToggle(500);
@@ -10,3 +10,24 @@
   });
 
 
+/* 3thPage works
+The frame is a flex box
+set their height the same as the that of the largest one  */
+
+(function() {
+  if (document.documentElement.clientWidth > 480) {
+    var frames = $(".frame");
+    var frameHeighArr = [];
+
+    for (var i = 0, len = frames.length; i < len; i++) {
+      frameHeighArr.push(frames[i].scrollHeight);
+    }
+
+    var maxHeight = Math.max(...frameHeighArr);
+    for (var i = 0, len = frames.length; i < len; i++) {
+      frames[i].style.height = maxHeight + "px";
+    }
+  } else {
+    return;
+  }
+})();
